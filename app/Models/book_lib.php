6e7a318a4fd1a0dd2b3b8f87cf44d7051cf655cc
Vooppig <nurkasultan25@gmail.com>
2022-12-01 15:25:24 +0800

    pp

--- app/Models/book_lib.php
+++ app/Models/book_lib.php
@@ -8,11 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class book_lib extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $table ="library_system_reserved_books";
-=======
     protected $table = "library_system_book";
->>>>>>> 85cb95d220cc2db67932435eda6ba080595e3548
     protected $primaryKey = "id";
     protected $fillable = ['title', 'author', 'flag', 'publisher', 'isbn', 'price'];
 }
